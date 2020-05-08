e306735f52642540b8172c338601d07095f0cf7d
TaronJsDev <taron.sargsyan20@gmail.com>
2020-05-09 00:54:11 +0400

    3edt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Modul from "./componentes/Modul";
 
-function App() {
+function App(propes) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Modul name={propes.arr[1]} />;
+      <Modul name={propes.arr[2]} />;
+      <Modul name={propes.arr[3]} />;
     </div>
   );
 }
---
+++ src/componentes/Modul.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Modul(propes) {
+  return (
+    <div>
+      <h1 className="h1">Hello my name is {propes.name}</h1>
+    </div>
+  );
+}
